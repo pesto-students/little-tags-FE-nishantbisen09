8506a15e00e8191078a26c6c15a617807e915da6
nishantbisen09 <nishantbisen09@gmail.com>
2021-03-01 20:45:44 +0530

    add product header in home page

--- src/i18n/messages/en-US.js
+++ src/i18n/messages/en-US.js
@@ -10,5 +10,6 @@ export default {
       'Login to get access to your Orders, Wishlists and your personalized settings',
     signInWithGoogle: 'Sign in with Google',
     featuredAdTitle: 'Experience your music like {lineBreak} never before',
+    featuredProducts: 'Featured Products',
   },
 };
