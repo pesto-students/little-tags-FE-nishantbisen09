23fb0c3628fe29e4ce7ce6d36795096f612254ce
Tejas H <tejas007tez@gmail.com>
2021-03-11 17:44:43 +0530

    Remove unused code

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,35 +3,15 @@ import SearchIcon from '@material-ui/icons/Search';
 import InputBase from '@material-ui/core/InputBase';
 import { FormattedMessage } from 'react-intl';
 import { useHistory } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
-
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import fuse from './FuseSearch';
 import useStyles from './searchStyles';
 
-const useStylesMore = makeStyles(theme => ({
-  root: {
-    position: 'relative',
-  },
-  dropdown: {
-    position: 'absolute',
-    top: 28,
-    right: 0,
-    left: 0,
-    zIndex: 1,
-    border: '1px solid',
-    padding: theme.spacing(1),
-    backgroundColor: theme.palette.background.paper,
-  },
-}));
-
 function Search() {
-  const moreClasses = useStylesMore();
   const classes = useStyles();
   const history = useHistory();
   const [searchQuery, setSearchQuery] = useState('');
@@ -80,7 +60,7 @@ function Search() {
       </div>
 
       <ClickAwayListener onClickAway={handleClickAway}>
-        <div className={moreClasses.root}>
+        <>
           <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSearch}>
             <FormattedMessage id="search">
               {placeHolderText => (
@@ -129,7 +109,7 @@ function Search() {
               </List>
             </div>
           ) : null}
-        </div>
+        </>
       </ClickAwayListener>
     </div>
   );
