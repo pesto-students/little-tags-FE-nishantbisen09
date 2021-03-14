ed44be4a6dc44d0d6b0198d1aff7f371cb801771
Tejas H <tejas007tez@gmail.com>
2021-03-15 05:20:27 +0530

    Removed script append

--- src/utilities/loadScript.js
+++ src/utilities/loadScript.js
@@ -1,13 +1,13 @@
 export default function loadScript(src) {
   return new Promise(resolve => {
-    const script = document.createElement('script');
-    script.src = src;
-    script.onload = () => {
-      resolve(true);
-    };
-    script.onerror = () => {
-      resolve(false);
-    };
-    document.body.appendChild(script);
+    // const script = document.createElement('script');
+    // script.src = src;
+    // script.onload = () => {
+    //   resolve(true);
+    // };
+    // script.onerror = () => {
+    //   resolve(false);
+    // };
+    // document.body.appendChild(script);
   });
 }
